a797c464d5656ddece62212637fcdb89849ddbb0
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2023-12-26 06:03:23 -0800

    Update ClassifiersBuild.m

--- ClassifiersBuild.m
+++ ClassifiersBuild.m
@@ -1,24 +1,51 @@
-%% Classifiers Training
+%% Load and Preprocess Data
+XSSTesting = readtable('XSSTesting.csv', 'VariableNamingRule', 'preserve'); % Preserve headers
+DatasetTest = XSSTesting;
+% Extract features and target variable
+TBL = table2array(DatasetTest(:,1:Si(1,2)-1));  % Convert features to numeric array
+ResponseVarName = DatasetTest.(DatasetTest.Properties.VariableNames{Si(1,2)});  % Extract target variable
 
-%ssetup='%setup.m';
-XSSTraining= readtable ('XSSTraining.csv');
+%% Training Classifiers  % **Add this section**
+% Load training data (adjust file path as needed)
+XSSTraining = readtable('XSSTraining.csv', 'VariableNamingRule', 'preserve');
+DatasetTrain = XSSTraining;
+trainFeatures = table2array(DatasetTrain(:,1:Si(1,2)-1));  % Extract training features
+trainResponseVarName = DatasetTrain.(DatasetTrain.Properties.VariableNames{Si(1,2)});  % Extract training labels
 
-Dataset = XSSTraining; % Training dataset name
-Si = size(Dataset);		% Dataset Size
+% Choose classifier type
+classifierType = 'SVML';  % Or any other supported type
 
+tic; % Start timer
 
-ScTBL = Dataset(:,1:Si(1,2)-1);  % Inputs
-ScResponseVarName = Dataset(:,Si(1,2));  % Output
-rng('default');
+% Train the selected classifier
+if strcmp(classifierType, 'SVML')
+    classifier = fitcsvm(trainFeatures, trainResponseVarName, 'KernelFunction', 'linear', 'BoxConstraint', 7);
+elseif strcmp(classifierType, 'SVMP')
+    classifier = fitcsvm(trainFeatures, trainResponseVarName, 'KernelFunction', 'polynomial', 'PolynomialOrder', 3, 'OutlierFraction', 0.10);
+elseif strcmp(classifierType, 'KNN')
+    classifier = fitcknn(trainFeatures, trainResponseVarName, 'NumNeighbors', 1);
+elseif strcmp(classifierType, 'RF')
+    classifier = TreeBagger(40, trainFeatures, trainResponseVarName, 'Method', 'classification');
+else
+    error('Invalid classifier type');
+end
 
-SVML = fitcsvm(ScTBL,ScResponseVarName,'KernelFunction','linear', 'BoxConstraint', 0.5);   % Support Vector Machine- Linear kernel Classifier
-SVMP = fitcsvm(ScTBL,ScResponseVarName,'KernelFunction','polynomial', 'OutlierFraction', 0.73); % Support Vector Machine- Polynomial kernel Classifier
-KNN = fitcknn (ScTBL,ScResponseVarName, 'NumNeighbors', 1);  %k-NN Classifier
-RF = TreeBagger(70,ScTBL,ScResponseVarName, 'OOBPrediction','On','Method','classification');   % Random Forest Classifier
+%% Testing Classifiers
+predictedLabels = predict(classifier, TBL);  % Use the trained model for testing
+PerformanceTime = toc; % Stop timer
 
-TBL2 = table2array(ScTBL);  % Inputs table for NN
-ResponseVarName2 = table2array(ScResponseVarName);  % Output for NN
+CM = confusionmat(ResponseVarName, predictedLabels);  % Create confusion matrix
 
-net = patternnet(10, 'trainbr');  % Creating the net
-NN = train(net,TBL2,ResponseVarName2);  % Training NN classifier
-clear TBL2 ResponseVarName2;    % Remove unnecessary files
+accuracy = (CM(1,1)+CM(2,2))/(sum(sum(CM)));  % Calculate accuracy
+precision = CM(1,1)/(CM(1,1)+CM(1,2));  % Calculate precision
+sensitivity = CM(1,1)/(CM(1,1)+CM(2,1));  % Calculate sensitivity
+specificity = CM(2,2)/(CM(2,2)+CM(1,2));  % Calculate specificity
+
+disp('Classifier Performance:')
+disp('-----------------------')
+disp(fprintf('Classifier Type:     %s', classifierType))
+disp(fprintf('Accuracy:            %.2f%%', accuracy * 100))
+disp(fprintf('Precision:           %.2f%%', precision * 100))
+disp(fprintf('Sensitivity:         %.2f%%', sensitivity * 100))
+disp(fprintf('Specificity:         %.2f%%', specificity * 100))
+disp(fprintf('Performance Time:    %.4f seconds', PerformanceTime))
