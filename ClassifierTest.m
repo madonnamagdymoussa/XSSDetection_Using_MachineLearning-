85a010b512351c674ca92da62d982b8f065fd708
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2023-12-26 06:04:23 -0800

    Update ClassifierTest.m

--- ClassifierTest.m
+++ ClassifierTest.m
@@ -1,46 +1,81 @@
-XSSTesting= readtable ('XSSTesting.csv');
-DatasetTest = XSSTesting;  % Dataset name
-TBL = DatasetTest(:,1:Si(1,2)-1);  % Inputs
-ResponseVarName = DatasetTest(:,Si(1,2)); % Output
 
-tic; % Starting Time Counter
+%% Load and Preprocess Data
+XSSTesting = readtable('XSSTesting.csv', 'VariableNamingRule', 'preserve'); % Preserve headers
+DatasetTest = XSSTesting;
+% Extract features and target variable
+TBL = table2array(DatasetTest(:,1:Si(1,2)-1));  % Convert features to numeric array
+ResponseVarName = DatasetTest.(DatasetTest.Properties.VariableNames{Si(1,2)});  % Extract target variable
 
-%% Section of testing classifiers, Remove comment from the selected classifier.
+%% Handle Missing Values
+TBL_nomissing = TBL(~ismissing(TBL(:,1)), :);  % Remove rows with missing values in "x1"
 
-% label = predict(SVML,TBL);
-% label = predict(SVMP,TBL);
-% label = predict(KNN,TBL);
+%% Testing Classifiers
+tic; % Start timer
+% ... (rest of the classifier selection and prediction code)
 
-% Section of testing Random Forest Classifier.
 
-% Label = predict(RF,TBL);
-% Label = str2num(cell2mat(Label));
+%% Testing Classifiers
 
-% Section of testing NN Classifier.
 
-XTest = TBL; 
-Label = NN(XTest); 
-output = Label; 
-performance = perform(NN,XTest,Label);
-output1 = round(output); %abs for intger number
-Label = output1;
+% Uncomment the line for the classifier you want to test
+%label = predict(SVML, TBL);
+ %label = predict(SVMP, TBL);
+% label = predict(KNN, TBL);
+% Label = predict(RF, TBL);
 
-PerformanceTime = toc; % Ending Time Counter
+classifierType = 'SVML';
+% Tuned linear SVM
+if strcmp(classifierType, 'SVML')
+    classifier = fitcsvm(trainFeatures, trainResponseVarName, 'KernelFunction', 'linear', 'BoxConstraint', 7);
 
+% Tuned polynomial SVM
+elseif strcmp(classifierType, 'SVMP')
+    classifier = fitcsvm(trainFeatures, trainResponseVarName, 'KernelFunction', 'polynomial', 'PolynomialOrder', 3, 'OutlierFraction', 0.10);
+
+% Tuned k-NN
+elseif strcmp(classifierType, 'KNN')
+    classifier = fitcknn(trainFeatures, trainResponseVarName, 'NumNeighbors', 1);
+
+% Tuned Random Forest
+elseif strcmp(classifierType, 'RF')
+    classifier = TreeBagger(40, trainFeatures, trainResponseVarName, 'Method', 'classification');  % Assuming TreeBagger is used for Random Forest
+end
+
+%% Load and Preprocess Data
+XSSTesting = readtable('XSSTesting.csv', 'VariableNamingRule', 'preserve'); % Preserve headers
+DatasetTest = XSSTesting;
+% Extract features and target variable
+TBL = table2array(DatasetTest(:,1:Si(1,2)-1));  % Convert features to numeric array
+ResponseVarName = DatasetTest.(DatasetTest.Properties.VariableNames{Si(1,2)});  % Extract target variable
+
+%% Handle Missing Values
+TBL_nomissing = TBL(~ismissing(TBL(:,1)), :);  % Remove rows with missing values in "x1"
+
+%% Testing Classifiers
+tic; % Start timer
+% ... (rest of the classifier selection and prediction code)
+predictedLabels = predict(classifier, TBL_nomissing);  % Use the data without missing values
+PerformanceTime = toc; % Stop timer
+
+
+%predictedLabels = predict(classifier, testFeatures);
+
+
+%% Evaluate Performance
 disp('Performance Results');
-CM = confusionmat(ResponseVarName,Label);
+CM = confusionmat(ResponseVarName, label);  % Should work now with consistent types
 disp(CM)
 accuracy = (CM(1,1)+CM(2,2))/(CM(1,1)+CM(1,2)+CM(2,1)+CM(2,2));
-disp('Accuracy')
+disp('Accuracy:')
 disp(accuracy*100)
-disp('Precision')
+disp('Precision:')
 dr = CM(1,1)/(CM(1,1)+CM(1,2));
 disp(dr*100)
+disp('Sensitivity:')
 Sensitivity = CM(1,1)/(CM(1,1)+CM(2,1));
-Specificity = CM(2,2)/(CM(2,2)+CM(1,2));
-disp('Sensitivity')
 disp(Sensitivity*100)
-disp('Specificity')
+disp('Specificity:')
+Specificity = CM(2,2)/(CM(2,2)+CM(1,2));
 disp(Specificity*100)
-disp('Timing');
+disp('Timing:')
 disp(PerformanceTime);
